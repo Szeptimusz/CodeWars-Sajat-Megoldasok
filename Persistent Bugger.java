e4236362fc9771ac076e0b9324ac4aa2ef631e10
Szeptimusz <43076594+Szeptimusz@users.noreply.github.com>
2019-07-29 12:18:57 +0200

    Update Persistent Bugger.java

--- Persistent Bugger.java
+++ Persistent Bugger.java
@@ -3,7 +3,9 @@ Feladat eredeti helye: https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
 
 FELADAT LEÍRÁSA:
 
-Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is 
+the number of times you must multiply the digits in num until you reach a single digit.
+
 For example:
 persistence(39) == 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
                       // and 4 has only one digit
